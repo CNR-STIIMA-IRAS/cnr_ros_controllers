810ea900597167eb6248a5e26766d97c24b8975b
nicola pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2021-03-29 16:17:51 +0200

    removed dummy_controller & impedance

--- cnr_joint_impedance_controller/src/cnr_joint_impedance_controller/cnr_joint_impedance_controller.cpp
+++
@@ -1,6 +0,0 @@
-#include <name_sorting/name_sorting.h>
-#include <cnr_joint_impedance_controller/cnr_joint_impedance_controller.h>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(cnr::control::JointImpedanceController , controller_interface::ControllerBase)
-
